4251d34faf7fc6bb79a17c0f42ec07418e3e625d
Lee Arnold <lee.arnoldiii@gmail.com>
2020-09-21 08:52:21 -0500

    movie api started crapping out so switched to jsonplaceholder api. have it rendering via ejs (never used before). next stage is to finish the styling on the page and pair with a db.

--- jsRefresh/api/movieAPI/app.js
+++ jsRefresh/api/movieAPI/app.js
@@ -2,10 +2,53 @@ const express = require('express')
 const app = express()
 const port = 3000
 const axios = require('axios')
+const { response } = require('express')
 
-app.get('/results', (req, res) => {
-    res.send('Hello, it works!')
-})
+app.set('view engine', 'ejs')
+
+app.get('/results', async (req, res) => {
+    try {
+        const result = await axios.get(
+            `https://jsonplaceholder.typicode.com/users`
+        );
+        console.log(result.data)
+        const users = result.data
+        
+        res.render('results', {
+          users  
+        });
+    } catch (error) {
+        console.log(error);
+        res.status(400).send('Error while getting list of users');
+    }
+});
+
+// app.get('/results', (req, res) => {
+//     users = axios.get('https://jsonplaceholder.typicode.com/users')
+//     .then(response => {
+//         console.log(response.data)
+//     });
+//     const names = users.data;
+//     res.render('results', {
+//         names
+//     })
+// })
+
+// app.get('/results', (req, res) => {
+//     axios.get('https://omdbapi.com/?s=california&apikey=thewdb')
+//     .then(function (response) {
+//         console.log(response.data['Search'][0])
+//         res.send(response.data)
+//         // res.render("results", {data: data}) 
+//     })
+//     .catch(function (error) {
+//         console.log(error);
+//     })
+//     .finally(function () {
+
+//     });
+    
+// })
 
 
 
